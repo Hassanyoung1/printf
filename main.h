c8f4be003c8e0895a3c9dc4461712ceb71bc3603
Innocent <innocentsagini16@gmail.com>
2023-04-27 01:55:30 -0700

    printf_functions updates

--- main.h
+++ main.h
@@ -9,19 +9,34 @@
 #include <limits.h>
 #include <unistd.h>
 
+#define UNUSED(x) (void)(x)
+#define BUFF_SIZE 1024
 
+/* flags */
+#define F_MINUS 1
+#define F_PLUS 2
+#define F_ZERO 4
+#define F_HASH 8
+#define F_SPACE 16
+
+/* sizes */
+#define S_LONG 2
+#define S_SHORT 1
+
+struct fmt
+{
+        char fmt;
+        int (*fn)(va_list, char[], int, int, int, int);
+};
 
 
 /**prototypes */
 
-typedef struct format
+typedef struct format;
 {
 	char *id;
 	int (*f)();
 } convert_match;
-
-int putchar(char c);
-int _printf(const char *format, ...);
 
 /**function prototype*/
 
@@ -36,6 +51,8 @@ int print_number(int n);
 int print_binary(unsigned int n);
 int _print_number(int n, char *buffer);
 int print_hex(unsigned long int num, int is_capital);
-int print_int(int n, int flags)
+int print_int(int n, int flags);
+int handle_print(const char *fmt, int *i,
+va_list list, char buffer[], int flags, int width, int precision, int size);
 
 #endif
